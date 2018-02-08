8644b2bc52d06da4e5d31d054284fc01d7aada02
nbaghiro <naib.baghirov@gmail.com>
2018-02-07 21:21:03 -0500

    react front end and stripe billing done

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,16 @@ passport.use(
 			callbackURL: "/auth/google/callback",
 			proxy: true
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id }).then(existingUser => {
-				if (existingUser) {
-					//We already have a record with the given Google ID
-					done(null, existingUser);
-				} else {
-					//We don't have a record with this ID, make a new record
-					new User({ googleId: profile.id }).save().then(user => {
-						done(null, user);
-					});
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id });
+
+			if (existingUser) {
+				done(null, existingUser);
+				return;
+			}
+
+			const user = await new User({ googleId: profile.id }).save();
+			done(null, user);
 		}
 	)
 );
